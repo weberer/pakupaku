88a127fe5a571d09f55ba57f536026cff01a5884
weberer <weberer@outlook.com>
2019-10-31 13:43:06 -0500

    Fixed issues with library refernces in GhostTest.java

--- reengineering/src/new/PakuJava/src/Model/KinkyTest.java
+++ reengineering/src/new/PakuJava/src/Model/KinkyTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 public class KinkyTest {
 
 
