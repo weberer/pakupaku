485c42d3896d42c586a750635998d1b8d4487dbe
weberer <weberer@outlook.com>
2019-11-01 14:43:42 -0500

    Final updates to UI

--- reengineering/src/new/PakuJava/web/js/ghost.js
+++ reengineering/src/new/PakuJava/web/js/ghost.js
@@ -1,13 +1,34 @@
 class Ghost extends MovingEntity {
     constructor(id) {
         super(id);
-        this.constructor.stylesheet = "ghost.css";
-        this.constructor.cssElementName = ".ghost";
+        this.stylesheet = "ghost.css";
+        //this.cssElementName = ".ghost";
+        console.log("#" + id);
+        this.cssElementname = "#" + id;
     }
 
     static attrNames = {
         direction: 'direction',
         state: 'state'
+    };
+
+    static startingPos = {
+        blaine: {
+            x: 15,
+            y: 13
+        },
+        hinky: {
+            x: 10,
+            y: 13
+        },
+        kinky: {
+            x: 12.5,
+            y: 13,
+        },
+        stinky: {
+            x: 12.5,
+            y: 10
+        }
     };
 
     // holds list of all ghosts
@@ -30,6 +51,25 @@ class Ghost extends MovingEntity {
         })
     };
 
+    /* Temp fix for movement updates*/
+    setX1 = (x) => {
+        let cssElement = Util.getCssElement("ghost.css", "#" + this.id);
+
+        if(cssElement.style && cssElement.style.getPropertyValue("--x_coord"))
+            cssElement.style.setProperty("--x_coord", x);
+        else
+            throw "Exception: Property " + "propertyName" + " does not exist";
+    };
+
+    setY1 = (x) => {
+        let cssElement = Util.getCssElement("ghost.css", "#" + this.id);
+
+        if(cssElement.style && cssElement.style.getPropertyValue("--y_coord"))
+            cssElement.style.setProperty("--y_coord", x);
+        else
+            throw "Exception: Property " + "propertyName" + " does not exist";
+    };
+
     // call in init();
     static createGhosts() {
         let stinky = new Ghost('stinky');
@@ -41,5 +81,15 @@ class Ghost extends MovingEntity {
         Ghost.addGhost(kinky);
         Ghost.addGhost(hinky);
         Ghost.addGhost(blaine);
+    }
+
+    static moveToStartingLocations = () => {
+        this.ghosts.forEach((ghost) => {
+            let name = ghost.id;
+            let x = this.startingPos[name].x;
+            let y = this.startingPos[name].y;
+            ghost.setX1(x);
+            ghost.setY1(y);
+        })
     }
 }
--- reengineering/src/new/PakuJava/web/js/networking.js
+++ reengineering/src/new/PakuJava/web/js/networking.js
@@ -17,8 +17,10 @@ class Networking {
     };
 
     static _sendRequest = (data, callback) => {
-        let url = window.location.href + this.vars.requestUrl;
+        let url = window.location.href + "servlet/PakuPaku";
+
         $.ajax({
+            //url: "http://localhost:8080/paku_war_exploded/servlet/PakuPaku",
             url: url,
             data: data,
             type: "POST",
@@ -43,11 +45,16 @@ class Networking {
     };
 
     static sendMenuRequest = () => {
+        console.log("Sending Menu Request");
         let data = {
             type: "menu"
         };
 
-        this._sendRequest(data, this.success);
+        this._sendRequest(data, (rawData) => {
+            let data = JSON.parse(rawData).data;
+            let highscoreList = data.highscore_list;
+            //Game.updateHighScoreList(highscoreList); // Backend returns array, not object as is needed to update the list
+        });
     };
 
     static sendStartGameRequest = () => {
