b0db2ec231856919db22aefcb286ac29fae4ea00
weberer <weberer@outlook.com>
2019-10-31 19:49:33 -0500

    Removed unused imports from ghost test that caused build errors

--- reengineering/src/new/PakuJava/src/test/KinkyTest.java
+++ reengineering/src/new/PakuJava/src/test/KinkyTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KinkyTest {
 
